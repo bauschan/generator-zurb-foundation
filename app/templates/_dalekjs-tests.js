cddd2e1fd16a1f689ccfd4017bc417227ee1a97b
Peter Foerger <peter.foerger@dkd.de>
2013-11-26 12:59:20 +0100

    Updated dependencies

--- app/templates/_dalekjs-tests.js
+++ app/templates/_dalekjs-tests.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // Checks if the <title> of ´github.com´ has the expected value
+    // Checks if the <title> of ´index.html´ has the expected value
     'Page title is correct': function (test) {
         'use strict';
         test
